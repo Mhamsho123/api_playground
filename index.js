b6a49c77c4ee31217a8baa68a323ea3dfe52cdda
Mohamad Hamsho <hamsho1030@gmail.com>
2026-05-24 00:38:51 -0400

    Adding error handlers and css

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 document.getElementById('search-btn').addEventListener('click',function(){
     const topicInputEl = document.getElementById('topic-input').value.toLowerCase().trim()
+    
 
+    if(topicInputEl == ""){
+        alert('Error')
+    }else{
+        
+        document.querySelector('.input-container').style.display = 'none'
+        document.querySelector('#search-bar').style.display = 'block'
 
-
-    fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${topicInputEl}`)
+        fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${topicInputEl}`)
         .then(res => res.json())
         .then(data =>{
             console.log(data)
@@ -12,19 +18,20 @@ document.getElementById('search-btn').addEventListener('click',function(){
             const {displaytitle, description, thumbnail, timestamp} = data
 
             html = `
-            <div>
-                ${displaytitle}
-            </div>
-            <div>
-                ${description}
-            </div>
-            <div>
-                <img src='${thumbnail.source}'>
-            </div>
-                ${timestamp}
-            </div>
-            `
-            document.getElementById('section-1').innerHTML= html
-        })
+            <div class="search-results-container">
+                <h1>${displaytitle}</h1>
         
+                <p class="description">${description}</p>
+        
+                <div class="img-container">
+                    <img src="${thumbnail.source}">
+                </div>
+        
+                <p class="timestamp">${timestamp}</p>
+            </div>
+        `
+            document.getElementById('results').innerHTML= html
+        })
+
+    }
 })
