457e3469da3259e73487dab26aa1c934b957768f
Mohamad Hamsho <hamsho1030@gmail.com>
2026-05-23 22:19:10 -0400

    Implementing functionality on retrieving data and displaying on the screen

--- index.js
+++ index.js
@@ -5,5 +5,26 @@ document.getElementById('search-btn').addEventListener('click',function(){
 
     fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${topicInputEl}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data =>{
+            console.log(data)
+            let html = ""
+
+            const {displaytitle, description, thumbnail, timestamp} = data
+
+            html = `
+            <div>
+                ${displaytitle}
+            </div>
+            <div>
+                ${description}
+            </div>
+            <div>
+                <img src='${thumbnail.source}'>
+            </div>
+                ${timestamp}
+            </div>
+            `
+            document.getElementById('section-1').innerHTML= html
+        })
+        
 })
